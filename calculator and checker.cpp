b36d762f063c2f4eae847c1dddae0653ec7673e7
CharlsDj <charlsdelante14@gmail.com>
2026-08-17 11:30:35 +0800

    Add files via upload
    grade calculator and checker if it passed or not

---
+++ calculator and checker.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+	float machine,written,laboratory,quiz,participation,grade,participationgrade,quizgrade,laboratorygrade,writtengrade,machinegrade;
+	string fname,lname,code;
+	
+	cout<<"STUDENT GRADE CALCULATOR"<<endl;
+	cout<<"   "<<"/n enter your studet code"<<endl;
+	cin>> code;
+	cout<<"enter your first name: "<<endl;
+	cin>> fname;
+	cout<<"enter your last name: "<<endl;
+	cin>> lname;
+	cout<<"enter your machine problem exam score: "<<endl;
+	cin>> machine;
+	cout<<"enter your written exam score: "<<endl;
+	cin>> written;
+	cout<<"enter your laboratory score: "<<endl;
+	cin>> laboratory;
+	cout<<"enter your quiz score: "<<endl;
+	cin>> quiz;
+	cout<<"enter you class participation score: "<<endl;
+	cin>> participation;
+	
+	machinegrade = machine*.25;
+	writtengrade = written*.20;
+	laboratorygrade = laboratory*.25;
+	quizgrade = quiz*.15;
+	participationgrade = participation*.15;
+	grade = participationgrade + quizgrade + laboratorygrade + writtengrade + machinegrade;
+	
+	
+	cout<<"your grade: "<<grade;
+	
+	if (grade >= 75)
+	{
+		cout<<"the grade: "<< grade <<" is passed";	
+	}
+	else
+	{
+		
+		cout<<"the grade: "<< grade<<" is failed";
+	}
+	
+	
+	
+	
+		
+}
