90719866b1bc03cdbc5892b377aabf2d3ad4131a
CharlsDj <charlsdelante14@gmail.com>
2026-08-17 11:29:59 +0800

    Add files via upload
    grade checker if it passed or not

---
+++ grade_passing_checker.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+	int grade;
+	
+	cout<<"enter your grade: ";
+	cin>>grade;
+	
+	if (grade >= 75)
+	{
+		cout<<"the grade: "<< grade <<" is passed";	
+	}
+	else
+	{
+		
+		cout<<"the grade: "<< grade<<" is failed";
+	}
+	
+	
+	
+	
+}
